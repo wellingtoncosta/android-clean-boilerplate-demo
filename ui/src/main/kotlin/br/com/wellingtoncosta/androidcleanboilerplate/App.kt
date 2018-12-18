9b12980c3433be2cdc549b748155a5dfe2f41419
Wellington Costa <wellington.costa128@gmail.com>
2018-12-18 17:52:06 -0200

    Configuring base url.

--- ui/src/main/kotlin/br/com/wellingtoncosta/androidcleanboilerplate/App.kt
+++ ui/src/main/kotlin/br/com/wellingtoncosta/androidcleanboilerplate/App.kt
@@ -1,6 +1,7 @@
 package br.com.wellingtoncosta.androidcleanboilerplate
 
 import android.app.Application
+import com.github.kittinunf.fuel.core.FuelManager
 
 /**
  * @author Wellington Costa on 17/12/2018.
@@ -9,6 +10,9 @@ class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        FuelManager.instance.basePath = "https://my-collections-api.herokuapp.com"
+
         // TODO configure Koin modules
     }
 
